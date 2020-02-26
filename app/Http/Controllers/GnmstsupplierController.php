8eaffa6daf0c41fb7d20a8b6e75aa15bece40692
Abe Wisnu <abewisnu@gmail.com>
2020-02-26 23:31:48 +0700

    Update Controller Part 3

--- app/Http/Controllers/GnmstsupplierController.php
+++ app/Http/Controllers/GnmstsupplierController.php
@@ -1,10 +1,160 @@
 <?php
+	
+	namespace App\Http\Controllers;
+	
+	use Illuminate\Http\Request;
+	
+	use App\Gnmstsupplier; 
+	use App\Gnmstsupplierbank; 
+	use App\Gnmstsupplierprofitcenter;
+	
+	class GnmstsupplierController extends Controller
+	{
+		public function add(Request $request, Gnmstsupplier $gnmstsupplier )
+		{
+			$this->validate($request, [
+            'CustomerCode' => 'required', 
+			]);
+			
+			$gnmstsupplier = $gnmstsupplier->create([
+	        'CompanyCode' => $request->CompanyCode,
+			'SupplierCode' => $request->SupplierCode,
+			'StandardCode' => $request->StandardCode,
+			'SupplierName' => $request->SupplierName,
+			'SupplierGovName' => $request->SupplierGovName,,
+			'Address1' => $request->Address1,
+			'Address2' => $request->Address2,
+			'Address3' => $request->Address3,
+			'Address4' => $request->Address4,
+			'PhoneNo' => $request->PhoneNo,
+			'HPNo' => $request->HPNo,
+			'FaxNo' => $request->FaxNo,
+			'ProvinceCode' => $request->ProvinceCode,
+			'AreaCode' => $request->AreaCode,
+			'CityCode' => $request->CityCode,
+			'ZipNo' => $request->ZipNo,
+			'isPKP' => $request->isPKP,
+			'NPWPNo' => $request->NPWPNo,
+			'NPWPDate' => $request->NPWPDate,
+			'Status' => $request->Status,
+			'CreatedBy' => $request->CreatedBy,
+			'CreatedDate' => $request->CreatedDate,
+			'LastUpdateBy' => $request->LastUpdateBy,
+			'LastUpdateDate' => $request->LastUpdateDate,
+			'isLocked' => $request->isLocked,
+			'LockingBy' => $request->LockingBy,
+			'LockingDate' => $request->LockingDate,
+			]);
 
-namespace App\Http\Controllers;
+			$gnmstsupplierbank = $gnmstsupplierbank->create([
+			'CompanyCode' => $request->CompanyCode, 
+			'SupplierCode' => $request->SupplierCode,
+			'BankCode' => $request->BankCode,
+			'BankName' => $request->BankName,
+			'AccountName' => $request->AccountName,
+			'AccountBank' => $request->AccountBank,
+			'CreatedBy' => $request->CreatedBy,
+			'CreatedDate' => $request->CreatedDate,
+			'LastUpdateBy' => $request->LastUpdateBy,
+			'LastUpdateDate' => $request->LastUpdateDate,
+			'isLocked' => $request->isLocked,
+			'LockingBy' => $request->LockingBy,
+			'LockingDate' => $request->LockingDate,
+			]);
 
-use Illuminate\Http\Request;
+			$gnmstsupplierprofitcenter = $gnmstsupplierprofitcenter->create([
+	        'CompanyCode' => $request->CompanyCode,
+			'BranchCode' => $request->BranchCode,
+			'SupplierCode' => $request->SupplierCode,
+			'ProfitCenterCode' => $request->ProfitCenterCode,
+			'ContactPerson' => $request->ContactPerson,
+			'SupplierClass' => $request->SupplierClass,
+			'SupplierGrade' => $request->SupplierGrade, 
+			'DiscPct' => $request->DiscPct,
+			'TOPCode' => $request->TOPCode,
+			'TaxCode' => $request->TaxCode,
+			'isBlackList' => $request->isBlackList,
+			'Status' => $request->Status,
+			'CreatedBy' => $request->CreatedBy,
+			'CreatedDate' => $request->CreatedDate,
+			'LastUpdateBy' => $request->LastUpdateBy,
+			'LastUpdateDate' => $request->LastUpdateDate,
+			]);
+			
+			return fractal()
+            ->item($gnmstcustomer)
+            ->transformWith(new SupplierTransformer)
+            ->toArray();
+			
+		}
+		
+		public function update(Request $request, Gnmstsupplier $gnmstsupplier)
+		{		
+			$gnmstsupplier->CompanyCode = $request->get('CompanyCode', $gnmstsupplier->CompanyCode);
+			$gnmstsupplier->SupplierCode = $request->get('SupplierCode', $gnmstsupplier->SupplierCode);
+			$gnmstsupplier->StandardCode = $request->get('StandardCode', $gnmstsupplier->StandardCode);
+			$gnmstsupplier->SupplierName = $request->get('SupplierName', $gnmstsupplier->SupplierName);
+			$gnmstsupplier->SupplierGovName = $request->get('SupplierGovName', $gnmstsupplier->SupplierGovName);
+			$gnmstsupplier->Address1 = $request->get('Address1', $gnmstsupplier->Address1);
+			$gnmstsupplier->Address2 = $request->get('Address2', $gnmstsupplier->Address2);
+			$gnmstsupplier->Address3 = $request->get('Address3', $gnmstsupplier->Address3);
+			$gnmstsupplier->Address4 = $request->get('Address4', $gnmstsupplier->Address4);
+			$gnmstsupplier->PhoneNo = $request->get('PhoneNo', $gnmstsupplier->PhoneNo);
+			$gnmstsupplier->HPNo  = $request->get('HPNo', $gnmstsupplier->HPNo);
+			$gnmstsupplier->FaxNo  = $request->get('FaxNo', $gnmstsupplier->FaxNo);
+			$gnmstsupplier->ProvinceCode  = $request->get('ProvinceCode', $gnmstsupplier->ProvinceCode);
+			$gnmstsupplier->AreaCode  = $request->get('AreaCode', $gnmstsupplier->AreaCode);
+			$gnmstsupplier->CityCode  = $request->get('CityCode', $gnmstsupplier->CityCode);
+			$gnmstsupplier->ZipNo  = $request->get('ZipNo', $gnmstsupplier->ZipNo);
+			$gnmstsupplier->isPKP  = $request->get('isPKP', $gnmstsupplier->isPKP);
+			$gnmstsupplier->NPWPNo  = $request->get('NPWPNo', $gnmstsupplier->NPWPNo);
+			$gnmstsupplier->NPWPDate  = $request->get('NPWPDate', $gnmstsupplier->NPWPDate);
+			$gnmstsupplier->Status  = $request->get('Status', $gnmstsupplier->Status);
+			$gnmstsupplier->CreatedBy  = $request->get('CreatedBy', $gnmstsupplier->CreatedBy);
+			$gnmstsupplier->CreatedDate  = $request->get('CreatedDate', $gnmstsupplier->CreatedDate);
+			$gnmstsupplier->LastUpdateBy  = $request->get('LastUpdateBy', $gnmstsupplier->LastUpdateBy);
+			$gnmstsupplier->LastUpdateDate  = $request->get('LastUpdateDate', $gnmstsupplier->LastUpdateDate);
+			$gnmstsupplier->isLocked  = $request->get('isLocked', $gnmstsupplier->isLocked);
+			$gnmstsupplier->LockingBy  = $request->get('LockingBy', $gnmstsupplier->LockingBy);
+			$gnmstsupplier->LockingDate  = $request->get('LockingDate', $gnmstsupplier->LockingDate);
+			$gnmstsupplier->save();
 
-class GnmstsupplierController extends Controller
-{
-    //
-}
+			$gnmstsupplierbank->CompanyCode = $request->get('CompanyCode', $gnmstsupplierbank->CompanyCode);
+			$gnmstsupplierbank->SupplierCode = $request->get('SupplierCode', $gnmstsupplierbank->SupplierCode);
+			$gnmstsupplierbank->BankCode = $request->get('BankCode', $gnmstsupplierbank->BankCode);
+			$gnmstsupplierbank->BankName = $request->get('BankName', $gnmstsupplierbank->BankName);
+			$gnmstsupplierbank->AccountName = $request->get('AccountName', $gnmstsupplierbank->AccountName);
+			$gnmstsupplierbank->AccountBank = $request->get('AccountBank', $gnmstsupplierbank->AccountBank);
+			$gnmstsupplierbank->CreatedBy = $request->get('CreatedBy', $gnmstsupplierbank->CreatedBy);
+			$gnmstsupplierbank->CreatedDate = $request->get('CreatedDate', $gnmstsupplierbank->CreatedDate);
+			$gnmstsupplierbank->LastUpdateBy = $request->get('LastUpdateBy', $gnmstsupplierbank->LastUpdateBy);
+			$gnmstsupplierbank->LastUpdateDate = $request->get('LastUpdateDate', $gnmstsupplierbank->LastUpdateDate);
+			$gnmstsupplierbank->isLocked = $request->get('isLocked', $gnmstsupplierbank->isLocked);
+			$gnmstsupplierbank->LockingBy = $request->get('LockingBy', $gnmstsupplierbank->LockingBy);
+			$gnmstsupplierbank->LockingDate = $request->get('LockingDate', $gnmstsupplierbank->LockingDate);
+			$gnmstsupplierbank->save();
+
+			$gnmstsupplierprofitcenter->CompanyCode = $request->get('CompanyCode', $gnmstsupplierprofitcenter->CompanyCode);
+			$gnmstsupplierprofitcenter->BranchCode = $request->get('BranchCode', $gnmstsupplierprofitcenter->BranchCode);
+			$gnmstsupplierprofitcenter->SupplierCode = $request->get('SupplierCode', $gnmstsupplierprofitcenter->SupplierCode);
+			$gnmstsupplierprofitcenter->ProfitCenterCode = $request->get('ProfitCenterCode', $gnmstsupplierprofitcenter->ProfitCenterCode);
+			$gnmstsupplierprofitcenter->ContactPerson = $request->get('ContactPerson', $gnmstsupplierprofitcenter->ContactPerson);
+			$gnmstsupplierprofitcenter->SupplierClass = $request->get('SupplierClass', $gnmstsupplierprofitcenter->SupplierClass);
+			$gnmstsupplierprofitcenter->SupplierGrade = $request->get('SupplierGrade', $gnmstsupplierprofitcenter->SupplierGrade);
+			$gnmstsupplierprofitcenter->DiscPct = $request->get('DiscPct', $gnmstsupplierprofitcenter->DiscPct);
+			$gnmstsupplierprofitcenter->TOPCode = $request->get('TOPCode', $gnmstsupplierprofitcenter->TOPCode);
+			$gnmstsupplierprofitcenter->TaxCode = $request->get('TaxCode', $gnmstsupplierprofitcenter->TaxCode);
+			$gnmstsupplierprofitcenter->isBlackList = $request->get('isBlackList', $gnmstsupplierprofitcenter->isBlackList);
+			$gnmstsupplierprofitcenter->Status = $request->get('Status', $gnmstsupplierprofitcenter->Status);
+			$gnmstsupplierprofitcenter->CreatedBy = $request->get('CreatedBy', $gnmstsupplierprofitcenter->CreatedBy);
+			$gnmstsupplierprofitcenter->CreatedDate = $request->get('CreatedDate', $gnmstsupplierprofitcenter->CreatedDate);
+			$gnmstsupplierprofitcenter->LastUpdateBy = $request->get('LastUpdateBy', $gnmstsupplierprofitcenter->LastUpdateBy);
+			$gnmstsupplierprofitcenter->LastUpdateDate = $request->get('LastUpdateDate', $gnmstsupplierprofitcenter->LastUpdateDate);
+			$gnmstsupplierprofitcenter->save();
+			
+			return fractal()
+            ->item($gnmstcustomer)
+            ->transformWith(new SupplierTransformer)
+            ->toArray();
+		}
+	}
--- app/Http/Controllers/SpmstitemController.php
+++ app/Http/Controllers/SpmstitemController.php
@@ -1,10 +1,274 @@
 <?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class SpmstitemController extends Controller
-{
-    //
-}
+	
+	namespace App\Http\Controllers;
+	
+	use Illuminate\Http\Request;
+	
+	use App\Spmstitem; 
+	use App\Spmstiteminfo; 
+	use App\Spmstitemloc;
+	use App\Spmstitemprice;
+	
+	class SpmstitemController extends Controller
+	{
+		public function add(Request $request, Spmstitem $spmstitem )
+		{
+			$this->validate($request, [
+            'CustomerCode' => 'required', 
+			]);
+			
+			$spmstitem = $spmstitem->create([
+	        'CompanyCode' => $request->CompanyCode
+			'BranchCode' => $request->BranchCode
+			'PartNo' => $request->PartNo
+			'MovingCode' => $request->MovingCode
+			'DemandAverage' => $request->DemandAverage
+			'BornDate' => $request->BornDate
+			'ABCClass' => $request->ABCClass
+			'LastDemandDate' => $request->LastDemandDate
+			'LastPurchaseDate' => $request->LastPurchaseDate
+			'LastSalesDate' => $request->LastSalesDate
+			'BOMInvAmt' => $request->BOMInvAmt
+			'BOMInvQty' => $request->BOMInvQty
+			'BOMInvCostPrice' => $request->BOMInvCostPrice
+			'OnOrder' => $request->OnOrder
+			'InTransit' => $request->InTransit
+			'OnHand' => $request->OnHand
+			'AllocationSP' => $request->AllocationSP
+			'AllocationSR' => $request->AllocationSR
+			'AllocationSL' => $request->AllocationSL
+			'BackOrderSP' => $request->BackOrderSP
+			'BackOrderSR' => $request->BackOrderSR
+			'BackOrderSL' => $request->BackOrderSL
+			'ReservedSP' => $request->ReservedSP
+			'ReservedSR' => $request->ReservedSR
+			'ReservedSL' => $request->ReservedSL
+			'BorrowQty' => $request->BorrowQty
+			'BorrowedQty' => $request->BorrowedQty
+			'SalesUnit' => $request->SalesUnit
+			'OrderUnit' => $request->OrderUnit
+			'OrderPointQty' => $request->OrderPointQty
+			'SafetyStockQty' => $request->SafetyStockQty
+			'LeadTime' => $request->LeadTime
+			'OrderCycle' => $request->OrderCycle
+			'SafetyStock' => $request->SafetyStock
+			'Utility1' => $request->Utility1
+			'Utility2' => $request->Utility2
+			'Utility3' => $request->Utility3
+			'Utility4' => $request->Utility4
+			'TypeOfGoods' => $request->TypeOfGoods
+			'Status' => $request->Status
+			'ProductType' => $request->ProductType
+			'PartCategory' => $request->PartCategory
+			'CreatedBy' => $request->CreatedBy
+			'CreatedDate' => $request->CreatedDate
+			'LastUpdateBy' => $request->LastUpdateBy
+			'LastUpdateDate' => $request->LastUpdateDate
+			'isLocked' => $request->isLocked
+			'LockingBy' => $request->LockingBy
+			'LockingDate' => $request->LockingDate
+			'PurcDiscPct' => $request->PurcDiscPct
+			]);
+			
+			$gnmstsupplierbank = $gnmstsupplierbank->create([
+			'CompanyCode' => $request->CompanyCode, 
+			'SupplierCode' => $request->SupplierCode,
+			'BankCode' => $request->BankCode,
+			'BankName' => $request->BankName,
+			'AccountName' => $request->AccountName,
+			'AccountBank' => $request->AccountBank,
+			'CreatedBy' => $request->CreatedBy,
+			'CreatedDate' => $request->CreatedDate,
+			'LastUpdateBy' => $request->LastUpdateBy,
+			'LastUpdateDate' => $request->LastUpdateDate,
+			'isLocked' => $request->isLocked,
+			'LockingBy' => $request->LockingBy,
+			'LockingDate' => $request->LockingDate,
+			]);
+			
+			$gnmstsupplierprofitcenter = $gnmstsupplierprofitcenter->create([
+	        'CompanyCode' => $request->CompanyCode,
+			'BranchCode' => $request->BranchCode,
+			'SupplierCode' => $request->SupplierCode,
+			'ProfitCenterCode' => $request->ProfitCenterCode,
+			'ContactPerson' => $request->ContactPerson,
+			'SupplierClass' => $request->SupplierClass,
+			'SupplierGrade' => $request->SupplierGrade, 
+			'DiscPct' => $request->DiscPct,
+			'TOPCode' => $request->TOPCode,
+			'TaxCode' => $request->TaxCode,
+			'isBlackList' => $request->isBlackList,
+			'Status' => $request->Status,
+			'CreatedBy' => $request->CreatedBy,
+			'CreatedDate' => $request->CreatedDate,
+			'LastUpdateBy' => $request->LastUpdateBy,
+			'LastUpdateDate' => $request->LastUpdateDate,
+			]);
+			
+			$spmstitemprice = $spmstitemprice->create([
+	        'CompanyCode' => $request->CompanyCode,
+			'BranchCode' => $request->BranchCode,
+			'PartNo' => $request->PartNo,
+			'RetailPrice' => $request->RetailPrice,
+			'RetailPriceInclTax' => $request->RetailPriceInclTax,
+			'PurchasePrice' => $request->PurchasePrice,
+			'CostPrice' => $request->CostPrice,
+			'OldRetailPrice' => $request->OldRetailPrice,
+			'OldPurchasePrice' => $request->OldPurchasePrice,
+			'OldCostPrice' => $request->OldCostPrice,
+			'LastPurchaseUpdate' => $request->LastPurchaseUpdate,
+			'LastRetailPriceUpdate' => $request->LastRetailPriceUpdate,
+			'CreatedBy' => $request->CreatedBy,
+			'CreatedDate' => $request->CreatedDate,
+			'LastUpdateBy' => $request->LastUpdateBy,
+			'LastUpdateDate' => $request->LastUpdateDate,
+			'isLocked' => $request->isLocked,
+			'LockingBy' => $request->LockingBy,
+			'LockingDate' => $request->LockingDate,
+			]);
+			
+			return fractal()
+            ->item($gnmstcustomer)
+            ->transformWith(new SupplierTransformer)
+            ->toArray();
+			
+		}
+		
+		public function update(Request $request, Spmstitem $spmstitem)
+		{	
+			$spmstitem->CompanyCode = $request->get('CompanyCode', $spmstitem->CompanyCode);
+			$spmstitem->BranchCode = $request->get('BranchCode', $spmstitem->BranchCode);
+			$spmstitem->PartNo = $request->get('PartNo', $spmstitem->PartNo);
+			$spmstitem->MovingCode = $request->get('MovingCode', $spmstitem->MovingCode);
+			$spmstitem->DemandAverage = $request->get('DemandAverage', $spmstitem->DemandAverage);
+			$spmstitem->BornDate = $request->get('BornDate', $spmstitem->BornDate);
+			$spmstitem->ABCClass = $request->get('ABCClass', $spmstitem->ABCClass);
+			$spmstitem->LastDemandDate = $request->get('LastDemandDate', $spmstitem->LastDemandDate);
+			$spmstitem->LastPurchaseDate = $request->get('LastPurchaseDate', $spmstitem->LastPurchaseDate);
+			$spmstitem->LastSalesDate = $request->get('LastSalesDate', $spmstitem->LastSalesDate);
+			$spmstitem->BOMInvAmt = $request->get('BOMInvAmt', $spmstitem->BOMInvAmt);
+			$spmstitem->BOMInvQty = $request->get('BOMInvQty', $spmstitem->BOMInvQty);
+			$spmstitem->BOMInvCostPrice = $request->get('BOMInvCostPrice', $spmstitem->BOMInvCostPrice);
+			$spmstitem->OnOrder = $request->get('OnOrder', $spmstitem->OnOrder);
+			$spmstitem->InTransit = $request->get('InTransit', $spmstitem->InTransit);
+			$spmstitem->OnHand = $request->get('OnHand', $spmstitem->OnHand);
+			$spmstitem->AllocationSP = $request->get('AllocationSP', $spmstitem->AllocationSP);
+			$spmstitem->AllocationSR = $request->get('AllocationSR', $spmstitem->AllocationSR);
+			$spmstitem->AllocationSL = $request->get('AllocationSL', $spmstitem->AllocationSL);
+			$spmstitem->BackOrderSP = $request->get('BackOrderSP', $spmstitem->BackOrderSP);
+			$spmstitem->BackOrderSR = $request->get('BackOrderSR', $spmstitem->BackOrderSR);
+			$spmstitem->BackOrderSL = $request->get('BackOrderSL', $spmstitem->BackOrderSL);
+			$spmstitem->ReservedSP = $request->get('ReservedSP', $spmstitem->ReservedSP);
+			$spmstitem->ReservedSR = $request->get('ReservedSR', $spmstitem->ReservedSR);
+			$spmstitem->ReservedSL = $request->get('ReservedSL', $spmstitem->ReservedSL);
+			$spmstitem->BorrowQty = $request->get('BorrowQty', $spmstitem->BorrowQty);
+			$spmstitem->BorrowedQty = $request->get('BorrowedQty', $spmstitem->BorrowedQty);
+			$spmstitem->SalesUnit = $request->get('SalesUnit', $spmstitem->SalesUnit);
+			$spmstitem->OrderUnit = $request->get('OrderUnit', $spmstitem->OrderUnit);
+			$spmstitem->OrderPointQty = $request->get('OrderPointQty', $spmstitem->OrderPointQty);
+			$spmstitem->SafetyStockQty = $request->get('SafetyStockQty', $spmstitem->SafetyStockQty);
+			$spmstitem->LeadTime = $request->get('LeadTime', $spmstitem->LeadTime);
+			$spmstitem->OrderCycle = $request->get('OrderCycle', $spmstitem->OrderCycle);
+			$spmstitem->SafetyStock = $request->get('SafetyStock', $spmstitem->SafetyStock);
+			$spmstitem->Utility1 = $request->get('Utility1', $spmstitem->Utility1);
+			$spmstitem->Utility2 = $request->get('Utility2', $spmstitem->Utility2);
+			$spmstitem->Utility3 = $request->get('Utility3', $spmstitem->Utility3);
+			$spmstitem->Utility4 = $request->get('Utility4', $spmstitem->Utility4);
+			$spmstitem->TypeOfGoods = $request->get('TypeOfGoods', $spmstitem->TypeOfGoods);
+			$spmstitem->Status = $request->get('Status', $spmstitem->Status);
+			$spmstitem->ProductType = $request->get('ProductType', $spmstitem->ProductType);
+			$spmstitem->PartCategory = $request->get('PartCategory', $spmstitem->PartCategory);
+			$spmstitem->CreatedBy = $request->get('CreatedBy', $spmstitem->CreatedBy);
+			$spmstitem->CreatedDate = $request->get('CreatedDate', $spmstitem->CreatedDate);
+			$spmstitem->LastUpdateBy = $request->get('LastUpdateBy', $spmstitem->LastUpdateBy);
+			$spmstitem->LastUpdateDate = $request->get('LastUpdateDate', $spmstitem->LastUpdateDate);
+			$spmstitem->isLocked = $request->get('isLocked', $spmstitem->isLocked);
+			$spmstitem->LockingBy = $request->get('LockingBy', $spmstitem->LockingBy);
+			$spmstitem->LockingDate = $request->get('LockingDate', $spmstitem->LockingDate);
+			$spmstitem->PurcDiscPct = $request->get('PurcDiscPct', $spmstitem->PurcDiscPct);
+			$spmstitem->save();
+			
+			
+			$spmstiteminfo->CompanyCode = $request->get('CompanyCode', $spmstiteminfo->CompanyCode);
+			$spmstiteminfo->PartNo = $request->get('PartNo', $spmstiteminfo->PartNo);
+			$spmstiteminfo->SupplierCode = $request->get('SupplierCode', $spmstiteminfo->SupplierCode);
+			$spmstiteminfo->PartName = $request->get('PartName', $spmstiteminfo->PartName);
+			$spmstiteminfo->IsGenuinePart = $request->get('IsGenuinePart', $spmstiteminfo->IsGenuinePart);
+			$spmstiteminfo->DiscPct = $request->get('DiscPct', $spmstiteminfo->DiscPct);
+			$spmstiteminfo->SalesUnit = $request->get('SalesUnit', $spmstiteminfo->SalesUnit);
+			$spmstiteminfo->OrderUnit = $request->get('OrderUnit', $spmstiteminfo->OrderUnit);
+			$spmstiteminfo->PurchasePrice = $request->get('PurchasePrice', $spmstiteminfo->PurchasePrice);
+			$spmstiteminfo->UOMCode = $request->get('UOMCode', $spmstiteminfo->UOMCode);
+			$spmstiteminfo->Status = $request->get('Status', $spmstiteminfo->Status);
+			$spmstiteminfo->ProductType = $request->get('ProductType', $spmstiteminfo->ProductType);
+			$spmstiteminfo->PartCategory = $request->get('PartCategory', $spmstiteminfo->PartCategory);
+			$spmstiteminfo->CreatedBy = $request->get('CreatedBy', $spmstiteminfo->CreatedBy);
+			$spmstiteminfo->CreatedDate = $request->get('CreatedDate', $spmstiteminfo->CreatedDate);
+			$spmstiteminfo->LastUpdateBy = $request->get('LastUpdateBy', $spmstiteminfo->LastUpdateBy);
+			$spmstiteminfo->LastUpdateDate = $request->get('LastUpdateDate', $spmstiteminfo->LastUpdateDate);
+			$spmstiteminfo->isLocked = $request->get('isLocked', $spmstiteminfo->isLocked);
+			$spmstiteminfo->LockingBy = $request->get('LockingBy', $spmstiteminfo->LockingBy);
+			$spmstiteminfo->LockingDate = $request->get('LockingDate', $spmstiteminfo->LockingDate);
+			$spmstiteminfo->save();
+			
+			$spmstitemloc->CompanyCode= $request->get('CompanyCode', $spmstitemloc->CompanyCode);
+			$spmstitemloc->BranchCode= $request->get('BranchCode', $spmstitemloc->BranchCode);
+			$spmstitemloc->PartNo= $request->get('PartNo', $spmstitemloc->PartNo);
+			$spmstitemloc->WarehouseCode= $request->get('WarehouseCode', $spmstitemloc->WarehouseCode);
+			$spmstitemloc->LocationCode= $request->get('LocationCode', $spmstitemloc->LocationCode);
+			$spmstitemloc->LocationSub1= $request->get('LocationSub1', $spmstitemloc->LocationSub1);
+			$spmstitemloc->LocationSub2= $request->get('LocationSub2', $spmstitemloc->LocationSub2);
+			$spmstitemloc->LocationSub3= $request->get('LocationSub3', $spmstitemloc->LocationSub3);
+			$spmstitemloc->LocationSub4= $request->get('LocationSub4', $spmstitemloc->LocationSub4);
+			$spmstitemloc->LocationSub5= $request->get('LocationSub5', $spmstitemloc->LocationSub5);
+			$spmstitemloc->LocationSub6= $request->get('LocationSub6', $spmstitemloc->LocationSub6);
+			$spmstitemloc->BOMInvAmount= $request->get('BOMInvAmount', $spmstitemloc->BOMInvAmount);
+			$spmstitemloc->BOMInvQty= $request->get('BOMInvQty', $spmstitemloc->BOMInvQty);
+			$spmstitemloc->BOMInvCostPrice= $request->get('BOMInvCostPrice', $spmstitemloc->BOMInvCostPrice);
+			$spmstitemloc->OnHand= $request->get('OnHand', $spmstitemloc->OnHand);
+			$spmstitemloc->AllocationSP= $request->get('AllocationSP', $spmstitemloc->AllocationSP);
+			$spmstitemloc->AllocationSR= $request->get('AllocationSR', $spmstitemloc->AllocationSR);
+			$spmstitemloc->AllocationSL= $request->get('AllocationSL', $spmstitemloc->AllocationSL);
+			$spmstitemloc->BackOrderSP= $request->get('BackOrderSP', $spmstitemloc->BackOrderSP);
+			$spmstitemloc->BackOrderSR= $request->get('BackOrderSR', $spmstitemloc->BackOrderSR);
+			$spmstitemloc->BackOrderSL= $request->get('BackOrderSL', $spmstitemloc->BackOrderSL);
+			$spmstitemloc->ReservedSP= $request->get('ReservedSP', $spmstitemloc->ReservedSP);
+			$spmstitemloc->ReservedSR= $request->get('ReservedSR', $spmstitemloc->ReservedSR);
+			$spmstitemloc->ReservedSL= $request->get('ReservedSL', $spmstitemloc->ReservedSL);
+			$spmstitemloc->Status= $request->get('Status', $spmstitemloc->Status);
+			$spmstitemloc->CreatedBy= $request->get('CreatedBy', $spmstitemloc->CreatedBy);
+			$spmstitemloc->CreatedDate= $request->get('CreatedDate', $spmstitemloc->CreatedDate);
+			$spmstitemloc->LastUpdateBy= $request->get('LastUpdateBy', $spmstitemloc->LastUpdateBy);
+			$spmstitemloc->LastUpdateDate= $request->get('LastUpdateDate', $spmstitemloc->LastUpdateDate);
+			$spmstitemloc->isLocked= $request->get('isLocked', $spmstitemloc->isLocked);
+			$spmstitemloc->LockingBy= $request->get('LockingBy', $spmstitemloc->LockingBy);
+			$spmstitemloc->LockingDate= $request->get('LockingDate', $spmstitemloc->LockingDate);
+			$spmstitemloc->save();
+			
+			$spmstitemprice->CompanyCode= $request->get('CompanyCode', $spmstitemprice->CompanyCode);
+			$spmstitemprice->BranchCode= $request->get('BranchCode', $spmstitemprice->BranchCode);
+			$spmstitemprice->PartNo= $request->get('PartNo', $spmstitemprice->PartNo);
+			$spmstitemprice->RetailPrice= $request->get('RetailPrice', $spmstitemprice->RetailPrice);
+			$spmstitemprice->RetailPriceInclTax= $request->get('RetailPriceInclTax', $spmstitemprice->RetailPriceInclTax);
+			$spmstitemprice->PurchasePrice= $request->get('PurchasePrice', $spmstitemprice->PurchasePrice);
+			$spmstitemprice->CostPrice= $request->get('CostPrice', $spmstitemprice->CostPrice);
+			$spmstitemprice->OldRetailPrice= $request->get('OldRetailPrice', $spmstitemprice->OldRetailPrice);
+			$spmstitemprice->OldPurchasePrice= $request->get('OldPurchasePrice', $spmstitemprice->OldPurchasePrice);
+			$spmstitemprice->OldCostPrice= $request->get('OldCostPrice', $spmstitemprice->OldCostPrice);
+			$spmstitemprice->LastPurchaseUpdate= $request->get('LastPurchaseUpdate', $spmstitemprice->LastPurchaseUpdate);
+			$spmstitemprice->LastRetailPriceUpdate= $request->get('LastRetailPriceUpdate', $spmstitemprice->LastRetailPriceUpdate);
+			$spmstitemprice->CreatedBy= $request->get('CreatedBy', $spmstitemprice->CreatedBy);
+			$spmstitemprice->CreatedDate= $request->get('CreatedDate', $spmstitemprice->CreatedDate);
+			$spmstitemprice->LastUpdateBy= $request->get('LastUpdateBy', $spmstitemprice->LastUpdateBy);
+			$spmstitemprice->LastUpdateDate= $request->get('LastUpdateDate', $spmstitemprice->LastUpdateDate);
+			$spmstitemprice->isLocked= $request->get('isLocked', $spmstitemprice->isLocked);
+			$spmstitemprice->LockingBy= $request->get('LockingBy', $spmstitemprice->LockingBy);
+			$spmstitemprice->LockingDate= $request->get('LockingDate', $spmstitemprice->LockingDate);
+			$spmstitemprice->save();
+			
+			return fractal()
+            ->item($gnmstcustomer)
+            ->transformWith(new SupplierTransformer)
+            ->toArray();
+		}		
+	}
