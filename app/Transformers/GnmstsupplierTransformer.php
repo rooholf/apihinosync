7ee77ef7fb3bab6a435a029128597f7fb9a51474
Ade Indra Saputra <dheendra7393@gmail.com>
2020-03-08 00:26:18 +0700

    update api supplier

--- app/Transformers/GnmstsupplierTransformer.php
+++ app/Transformers/GnmstsupplierTransformer.php
@@ -2,12 +2,13 @@
 
 namespace App\Transformers;
 
+use App\Gnmstsupplier;
 use League\Fractal\TransformerAbstract;
 
 class GnmstsupplierTransformer extends TransformerAbstract
 {
     
-    public function transform()
+    public function transform(Gnmstsupplier $gnmstsupplier)
     {
         return [
             'SupplierCode' => $gnmstcustomer->CompanyCode,
