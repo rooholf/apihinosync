4120af77c3dd42aa4639aab9a73183672326d795
Ade Indra Saputra <dheendra7393@gmail.com>
2020-04-14 03:47:19 +0700

    recieve

--- app/Sptrnprcvhdr.php
+++ app/Sptrnprcvhdr.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Sptrnprcvhdr extends Model
 {
     protected $table = 'spTrnPRcvHdr';
-    protected $primaryKey = ['CompanyCode', 'BranchCode', 'WRSNo'];
+    protected $primaryKey = 'WRSNo';
     public $incrementing = false;
     public $timestamps = false;
 
