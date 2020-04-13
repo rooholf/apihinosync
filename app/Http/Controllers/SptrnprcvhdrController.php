4c59a4fddfa91b313862bbc0bb1518da700dc267
Ade Indra Saputra <dheendra7393@gmail.com>
2020-04-14 03:01:54 +0700

    recieve

--- app/Http/Controllers/SptrnprcvhdrController.php
+++ app/Http/Controllers/SptrnprcvhdrController.php
@@ -52,41 +52,40 @@ class SptrnprcvhdrController extends Controller
             $branchcode = '002';
         }
 
+        // code
+        $gnmstdocument = Gnmstdocument::where('DocumentType', 'WRL')
+                                    ->where('BranchCode', $branchcode)
+                                    ->first();
+
+        $gnmstdocument2 = Gnmstdocument::where('DocumentType', 'BNL')
+                                    ->where('BranchCode', $branchcode)
+                                    ->first();
+
+        $gnmstdocument3 = Gnmstdocument::where('DocumentType', 'POS')
+                                    ->where('BranchCode', $branchcode)
+                                    ->first();
+
+        // thn wrl
+        $no1 = $gnmstdocument->DocumentSequence + 1;
+        $thnwrl = substr($gnmstdocument->DocumentYear, 2, 2);
+        $nourut = sprintf("%06s", $no1);
+
+        $no2 = $gnmstdocument2->DocumentSequence + 1;
+        $thnbnl = substr($gnmstdocument2->DocumentYear, 2, 2);
+        $nourut2 = sprintf("%06s", $no2);
+
+        $no3 = $gnmstdocument3->DocumentSequence + 1;
+        $thnpos = substr($gnmstdocument3->DocumentYear, 2, 2);
+        $nourut3 = sprintf("%06s", $no3);
+
+        $wrsno = 'WRL/'.$thnwrl.'/'.$nourut;
+        $binningno = 'BNL/'.$thnbnl.'/'.$nourut2;
+        $docno = 'POS/'.$thnpos.'/'.$nourut3;
+
+
         $header = Sptrnprcvhdr::where('GRNo','=', $request->GRNo)->first();
-
         // dd($header);
-
         if ($header === null) {
-            
-            $gnmstdocument = Gnmstdocument::where('DocumentType', 'WRL')
-                                        ->where('BranchCode', $branchcode)
-                                        ->first();
-
-            $gnmstdocument2 = Gnmstdocument::where('DocumentType', 'BNL')
-                                        ->where('BranchCode', $branchcode)
-                                        ->first();
-
-            $gnmstdocument3 = Gnmstdocument::where('DocumentType', 'POS')
-                                        ->where('BranchCode', $branchcode)
-                                        ->first();
-
-            // thn wrl
-            $no1 = $gnmstdocument->DocumentSequence + 1;
-            $thnwrl = substr($gnmstdocument->DocumentYear, 2, 2);
-            $nourut = sprintf("%06s", $no1);
-
-            $no2 = $gnmstdocument2->DocumentSequence + 1;
-            $thnbnl = substr($gnmstdocument2->DocumentYear, 2, 2);
-            $nourut2 = sprintf("%06s", $no2);
-
-            $no3 = $gnmstdocument3->DocumentSequence + 1;
-            $thnpos = substr($gnmstdocument3->DocumentYear, 2, 2);
-            $nourut3 = sprintf("%06s", $no3);
-
-            $wrsno = 'WRL/'.$thnwrl.'/'.$nourut;
-            $binningno = 'BNL/'.$thnbnl.'/'.$nourut2;
-            $docno = 'POS/'.$thnpos.'/'.$nourut3;
-
             Gnmstdocument::where('DocumentType', 'WRL')
             ->where('BranchCode', $branchcode)
             ->where('CompanyCode', $request->CompanyCode)
@@ -103,42 +102,34 @@ class SptrnprcvhdrController extends Controller
             ->update(['DocumentSequence' => $no3]);
 
             // echo $wrsno;die;
-
-            $spthdr = Sptrnprcvhdr::where('GRNo','=', $request->GRNo)
-                                    ->where('BranchCode','=',$branchcode)
-                                    ->where('CompanyCode','=', $request->CompanyCode)->first();
-
-            if (!$spthdr) {
-                $sptrnprcvhdr = Sptrnprcvhdr::firstOrCreate([
-                    'CompanyCode'=> $request->CompanyCode,
-                    'BranchCode'=> $branchcode,
-                    'WRSNo'=> $wrsno,
-                    'WRSDate'=> Carbon::now(),
-                    'BinningNo'=> $binningno,
-                    'BinningDate'=> Carbon::now(),
-                    'ReceivingType'=> $request->ReceivingType,
-                    'DNSupplierNo'=> $docno,
-                    'DNSupplierDate'=> Carbon::now(),
-                    'TransType'=> $request->TransType,
-                    'SupplierCode'=> $request->SupplierCode,
-                    'ReferenceNo'=> $request->ReferenceNo,
-                    'ReferenceDate'=> Carbon::now(),
-                    'TotItem'=> $request->TotItem,
-                    'TotWRSAmt'=> $request->TotWRSAmt,
-                    'Status'=> $request->Status,
-                    'PrintSeq'=> $request->PrintSeq,
-                    'TypeOfGoods'=> $request->TypeOfGoods,
-                    'CreatedBy'=> $request->CreatedBy,
-                    'CreatedDate'=> Carbon::now(),
-                    'LastUpdateBy'=> $request->LastUpdateBy,
-                    'LastUpdateDate'=> Carbon::now(),
-                    'isLocked'=> $request->isLocked,
-                    'LockingBy'=> $request->LockingBy,
-                    'LockingDate'=> Carbon::now(),
-                    'GRNo' => $request->GRNo,
-                ]);
-            }
-     
+            $sptrnprcvhdr = Sptrnprcvhdr::firstOrCreate([
+                'CompanyCode'=> $request->CompanyCode,
+                'BranchCode'=> $branchcode,
+                'WRSNo'=> $wrsno,
+                'WRSDate'=> Carbon::now(),
+                'BinningNo'=> $binningno,
+                'BinningDate'=> Carbon::now(),
+                'ReceivingType'=> $request->ReceivingType,
+                'DNSupplierNo'=> $docno,
+                'DNSupplierDate'=> Carbon::now(),
+                'TransType'=> $request->TransType,
+                'SupplierCode'=> $request->SupplierCode,
+                'ReferenceNo'=> $request->ReferenceNo,
+                'ReferenceDate'=> Carbon::now(),
+                'TotItem'=> $request->TotItem,
+                'TotWRSAmt'=> $request->TotWRSAmt,
+                'Status'=> $request->Status,
+                'PrintSeq'=> $request->PrintSeq,
+                'TypeOfGoods'=> $request->TypeOfGoods,
+                'CreatedBy'=> $request->CreatedBy,
+                'CreatedDate'=> Carbon::now(),
+                'LastUpdateBy'=> $request->LastUpdateBy,
+                'LastUpdateDate'=> Carbon::now(),
+                'isLocked'=> $request->isLocked,
+                'LockingBy'=> $request->LockingBy,
+                'LockingDate'=> Carbon::now(),
+                'GRNo' => $request->GRNo,
+            ]);
                 
 
             
