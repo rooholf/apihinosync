21e1155153397904586c72dff58a2456452ce253
Ade Indra Saputra <dheendra7393@gmail.com>
2020-04-18 05:41:26 +0700

    upmaster

--- app/Http/Controllers/SptrnprcvhdrController.php
+++ app/Http/Controllers/SptrnprcvhdrController.php
@@ -83,7 +83,7 @@ class SptrnprcvhdrController extends Controller
             $docno = $this->noUrut('POS', $branchcode, $request->CompanyCode);
             $hppno = $this->noUrut('HPP', $branchcode, $request->CompanyCode);
             // echo $wrsno;die;
-            $sptrnprcvhdr = Sptrnprcvhdr::firstOrNew([
+            $sptrnprcvhdr = Sptrnprcvhdr::firstOrCreate([
                 'CompanyCode'=> $request->CompanyCode,
                 'BranchCode'=> $branchcode,
                 'WRSNo'=> $wrsno,
